9b884a0deebcd91563cc31d71c1cfae183a04ff6
Bushings <skventura39@gmail.com>
2025-03-18 23:03:07 -0500

    Changed code

--- public/src/main.py
+++ public/src/main.py
@@ -1,3 +1,7 @@
-def main(): {
-    print("Hello World ")
-}
+from textnode import TextNode, TextType
+
+def main():
+    test = TextNode("Anchor text", "link", "www.boot.dev")
+    print(test.__repr__())
+
+main()
